0eb1a0c94df9cad3d47e56903358e77331fec40c
NuaeserTasin500 <osman1711362@gmail.com>
2025-12-25 23:09:06 +0600

    Create 010. String.py

---
+++ 010. String.py
@@ -0,0 +1,31 @@
+## String
+
+name = "John"
+friend = "Steve"
+print("The friend of", name, "is", friend)
+print()
+
+#new line consumption in string variable
+Poem = '''Twinkle Twinkle, Little Star
+How I wonder what you are!
+Up above the also high
+Like a diamond in the sky
+'''
+print(Poem)
+print()
+
+#String's considered as array. So we can print every individual character by using []
+person = "Jason"
+print(person[0]) #first character of string is considered as 0
+print(person[1])
+print(person[2])
+print(person[3])
+print(person[4])
+print()
+
+#new line also considered as character.
+twoperson = '''Jason
+Malik'''
+print(twoperson[4]) #print n
+print(twoperson[5]) #print newline
+print(twoperson[6]) #print M
