e21a1eca0283a2c7302cd0486c11f111aacab8a2
NuaeserTasin500 <osman1711362@gmail.com>
2025-12-25 21:38:36 +0600

    Create 004. Escape Sequence and Seperator.py

---
+++ 004. Escape Sequence and Seperator.py
@@ -0,0 +1,15 @@
+## Printing single quotation and double quotation
+print('Hey, I am a \'good boy\'')
+print('He said, \"You are a good person\"')
+
+## Printing next line 
+print("I am \n a good person")
+
+## Printing backslash (\)
+print("I am a good boy\\girl") 
+
+## Seperator
+print("Hello", "How", "Are", "You") #default seperator is space
+print("Hello", "How", "Are", "You", sep="~")
+print("Hello", "How", "Are", "You", sep="#")
+print('Hello', "How", "Are", "You", sep="$")
