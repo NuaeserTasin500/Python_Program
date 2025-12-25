a15a1ab85ad663c0b722f0514d55e9ed1d3bb2ed
NuaeserTasin500 <osman1711362@gmail.com>
2025-12-25 16:48:19 +0600

    Create 003. HelloWorld.py

---
+++ 003. HelloWorld.py
@@ -0,0 +1,19 @@
+### We can write comment by using '#'
+# print executes and then makes new line
+
+print("Hello World") #string print
+print(5) #integer print
+print(5.9) #float print
+print(3+6j) #complex number print
+print(['banana', 'apple', 'orrange']) #list print
+print((10, 20)) #tuple print
+print(range(6)) #range print
+print({'name': 'John', 'age':'35'}) #dictionary print
+print({1, 2, 3, 4}) #set print
+print(frozenset([1, 2, 3])) #frozenset print
+print(True) #boolean print
+print()
+
+## print without new line
+print('Hello ', end='')
+print('World', end='')
