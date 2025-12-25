c2fd15de64843b58ab5c6c26b61c11dfe75e88ed
NuaeserTasin500 <osman1711362@gmail.com>
2025-12-25 22:45:12 +0600

    Create 008. Datatype conversion.py

---
+++ 008. Datatype conversion.py
@@ -0,0 +1,36 @@
+## Datatype conversion (aka Typecasting)
+
+#Converting string into int
+a = "1"
+b = "2"
+c = a + b
+print(c) #result won't be 3, rather it will be 12 and that is string 
+
+d = int(a) #converting string 1 as integer 1
+e = int(b) #converting string 2 as integer 2
+f = d + e
+print(f) #result will be 3, and that is integer
+
+
+g = int(c) 
+print(g) #result is 12 and that is integer
+
+
+print()
+
+
+#Converting int into string
+a1 = 1 #integer
+b1 = str(a1) #converting integer 1 as string 1
+print(b1)
+
+print()
+
+
+#other datatype conversion
+print(float(123)) #integer to float
+print(str(12.43)) #float to string
+print(bool("True")) #string to boolean
+print(complex(123)) #integer to complex
+print(str(3+7j)) #complex to string
+#and so on.....
