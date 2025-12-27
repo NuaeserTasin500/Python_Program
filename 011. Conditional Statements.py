fb774d10c1d78856f23ea247049b3cc3adf6db02
NuaeserTasin500 <osman1711362@gmail.com>
2025-12-27 07:00:23 +0600

    Create 011. Conditional Statements.py

---
+++ 011. Conditional Statements.py
@@ -0,0 +1,12 @@
+## Conditional statements
+
+# Any condition is boolean and printing condition shows boolean.
+
+a = 18
+print("a =", a)
+print("a > 5 is", a > 5) #prints 'a > 5 is True' because 18 > 5
+print("a < 5 is", a < 5) #prints 'a < 5 is False' because 18 > 5
+print("a >= 5 is", a >= 5) #prints 'a >= 5 is True' because 18 > 5
+print("a <= 5 is", a <= 5) #prints 'a <= 5 is True' because 18 > 5
+print("a = 5 is", a == 5) #prints 'a = 5 is False' because 18 != 5 
+print("a != 5 is", a != 5) #prints 'a !=5 is True' because 18 != 5
