c516624ac0968e82845ab513cf58ba8dccb727d9
NuaeserTasin500 <osman1711362@gmail.com>
2025-12-27 18:31:48 +0600

    Create 015. Increment and Decrement.py

---
+++ 015. Increment and Decrement.py
@@ -0,0 +1,14 @@
+## increment and decrement
+
+# increment 
+x = 10
+print(x) #prints 10
+x = x + 1 #increment x with 1 that makes x = x + 1 = 10 + 1 = 11, so x = 11
+print(x) #prints 11
+
+# decrement
+x = 10 #keep x variable's value 10 again
+x = x - 1 #decrement x with 1 that makes x = x - 1 = 10 - 1 = 9, so x = 9
+print(x) #prints 9
+
+#in python, x++, ++x, x--, --x are not supported
