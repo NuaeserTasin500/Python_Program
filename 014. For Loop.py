8b8d00ab3a6fc14b085d58179906e48c0a7466e4
NuaeserTasin500 <osman1711362@gmail.com>
2025-12-27 07:40:29 +0600

    Create 014. For Loop.py

---
+++ 014. For Loop.py
@@ -0,0 +1,28 @@
+## For loop
+
+#same as for(int i=0; i<5; i++)
+for i in range(5): 
+    print(i)
+print()
+
+#same as for(int j=4; j<9; j++)
+for j in range(4, 9): 
+    print(j)
+print()
+
+#same as for(int k=4; k<20; k=k+2)
+for k in range(4, 20, 2): 
+    print(k)
+print()
+
+
+#printing a string's every letters
+name = 'Dalton'
+for m in name:
+    print(m)
+print()
+
+#printing every element of a list
+color = ['Red', 'Green', 'Blue']
+for x in color:
+    print(x)
