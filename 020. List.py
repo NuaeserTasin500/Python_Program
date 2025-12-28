30744e00362079d272ecd74c7d93f819bab3d0fd
NuaeserTasin500 <osman1711362@gmail.com>
2025-12-28 17:45:37 +0600

    Create 020. List.py

---
+++ 020. List.py
@@ -0,0 +1,110 @@
+## List
+
+list1 = [3, 5, 6] #this is list1
+print(list1)
+print(type(list1))
+print(len(list1)) #length of list1, which is 3
+print()
+
+#printing individual elements of list by index
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[-1]) #here list1[-1] = list1[len(list1)-1] = list1[3-1] = list[2]
+print() 
+
+# List can contain different datatype values
+list2 = [3, 3.5, "Hello", True]
+print(list2)
+print()
+
+# element manipulation
+if "Hello" in list2:
+    print("Yes") #obviously prints "Yes" because "Hello" is in list2
+else:
+    print("No")
+print()
+
+#using for loop for printing all elements of list
+department = ['CSE', 'EEE', 'ME', 'BBA']
+for x in department:
+    print(x) 
+print()
+
+#indexing
+print(department[:]) #prints whole list with all elements
+print(department[:3]) #prints ['CSE', 'EEE', 'ME'] 
+print(department[::2]) #prints ['CSE', 'ME'], here 2 is jump index like i = i+2in
+print()
+
+#adding elements in list
+list3 = [1, 2, 3, 4]
+print("list3 is = ", list3)
+list3.append(5)
+print("after adding 5, now list3 is = ", list3)
+
+#sorting elements in list (ascending order)
+list4 = [11, 100, 5, 45, 33, 50]
+print("Before sorting list: ", list4)
+list4.sort()
+print("After sorting list: ", list4)
+print()
+
+#sorting elements in list (descending order)
+list4 = [11, 100, 5, 45, 33, 50]
+print("Before sorting list: ", list4)
+list4.sort(reverse=True)
+print("After sorting list: ", list4)
+print()
+
+#we can store append(), sort() etc method applied lists, which prevents change of original list
+original_list4 = [11, 100, 5, 45, 33, 50]
+add_list4 = original_list4.append(70)
+asc_list4 = original_list4.sort()
+dsc_list4 = original_list4.sort(reverse=True)
+print("Original List = ", original_list4)
+print("Add 70 in this list = ", add_list4)
+print("Ascending order list = ", asc_list4)
+print("Descending order list = ", dsc_list4)
+print()
+
+#mirror elements in list
+list4 = [11, 100, 5, 45, 33, 50]
+mirror_list4 = list4.reverse()  
+print("The mirror of", list4, "is", mirror_list4)
+print()
+
+#getting index of any elements
+list5 = [3, 4, 5, 6]
+print(list5)
+print("The index of 3 is", list5.index(3))
+print("The index of 4 is", list5.index(4))
+print("The index of 5 is", list5.index(5))
+print("The index of 6 is", list5.index(6))
+print()
+
+#count elements
+list5 = [4, 5, 6, 4, 7, 5, 4, 0, 6, 5, 4]
+print(list5)
+print("In this list, 4 occurs", list5.count(4), "times")
+print("In this list, 5 occurs", list5.count(5), "times")
+print("In this list, 6 occurs", list5.count(6), "times")
+print("In this list, 7 occurs", list5.count(7), "times")
+print()
+
+#keeping list into another variable
+m = list5
+print(m)
+print()
+
+# adding elements by insert method
+m.insert(1, 899) #insert(index, element)
+print(m) #index 1 has 899
+print()
+
+# merging two lists
+list_a = [1, 2, 3, 4]
+list_b = [5, 6, 7, 8]
+list_c = list_a + list_b
+print(list_c)
+print()
