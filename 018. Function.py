0d8fb1736bfc179e5611e3d1b90eec68440580a1
NuaeserTasin500 <osman1711362@gmail.com>
2025-12-30 12:51:06 +0600

    Update 018. Function.py

--- 018. Function.py
+++ 018. Function.py
@@ -10,8 +10,8 @@ def add(x, y):
     return add_result
 
 def mul(x, y):
-    mul_result = x * y
-    return mul_result
+    return x * y #you can return result of mathematical calculation without using variable
+
 
 
 
@@ -29,4 +29,14 @@ thank_you()
 #Just notice this, I don't need to write "Thank you for this proceed." sentence several times,
 #rather I have just used thank_you function.
 #also I have easily interpreted (a*b)*(a+b) as mul(mul(a,b), add(a,b))
-#this is the advantages of using functions 
+#these are the advantages of using functions 
+
+
+#if I make another function here then it won't be considered as a part of main function
+def cheers():
+    return "You are good in mathematics" #return string without print
+
+#after whatever I write will be considered as main function
+print(cheers())
+
+#it means you can make function everywhere
