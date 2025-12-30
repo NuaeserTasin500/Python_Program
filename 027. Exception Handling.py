4971deca0e179baa89bbe6f260eeaf8e45487e7b
NuaeserTasin500 <osman1711362@gmail.com>
2025-12-30 20:54:21 +0600

    Update 027. Exception Handling.py

--- 027. Exception Handling.py
+++ 027. Exception Handling.py
@@ -19,4 +19,23 @@ try:
 except ValueError:
     print("Invalid input")
 except IndexError:
-    print("Index error") 
+    print("Index error")
+print()
+
+
+# Finally clause
+#in finally clause, it works whatever error or not.
+try:
+    a = [1, 2, 3, 4, 5, 6, 7, 8]
+    print(a)
+    index = int(input("Input 0~7:"))
+    print(a[index])
+except ValueError:
+    print("Invalid input")
+except IndexError:
+    print("Index error")
+finally:
+    print("Thank you")
+print()
+#if try, except, finally are into function, still finally parts will be executed without return function 
+    
