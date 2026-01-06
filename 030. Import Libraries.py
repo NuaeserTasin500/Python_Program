d96b764568a63331f14cc4d1bd3438b1f6b96318
NuaeserTasin500 <osman1711362@gmail.com>
2026-01-06 12:51:24 +0600

    Create 030. Import Libraries.py

---
+++ 030. Import Libraries.py
@@ -0,0 +1,14 @@
+## Import Libraries
+# In Python, there are several libraries which can makes our work easy. For example: Math library. 
+
+
+import math
+
+result = math.sqrt(9)
+print(result) #prints 3
+
+#if you want only sqrt, then we write "from math import sqrt". 
+#Then "result = math.sqrt(9)" will be "result = sqrt(9)"
+
+# for library shortation, "import math" can be written as "import math as mt"
+#Then "result = math.sqrt(9)" will be "result = mt.sqrt(9)"
