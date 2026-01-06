41e1a7411495158a2f89ca219beabc8ed849fdc3
NuaeserTasin500 <osman1711362@gmail.com>
2026-01-06 13:37:59 +0600

    Create 031. OS module.py

---
+++ 031. OS module.py
@@ -0,0 +1,9 @@
+## OS module
+# In Python, we can create, delete file and folders in a directory by OS module
+
+import os
+
+print("Current working directory is: ", os.getcwd()) #Shows current working directory
+os.mkdir("data") #folder created
+
+#for knowing more about os module, visit this playlist: https://youtube.com/playlist?list=PL1_UFEFkZ-eaoB6bWr0icnOID0-ppD29_&si=ePL531M8sOmiW6z5
