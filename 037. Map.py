5da1ba28771e1333a7aa74339b81c6e3b8d611e2
NuaeserTasin500 <osman1711362@gmail.com>
2026-01-27 22:38:55 +0600

    Update print statements to show float results

--- 037. Map.py
+++ 037. Map.py
@@ -22,11 +22,25 @@ print(c) #prints '<map object at 0x%SOMETHING%>'
 func_a = lambda x: x / 10
 list_a = [10, 20, 30, 40, 50]
 list_b = list(map(func_a, list_a)) 
-print(list_b) #prints '[1, 2, 3, 4, 5]'
+print(list_b) #prints '[1.0, 2.0, 3.0, 4.0, 5.0]'
 
 
 # Using anonymous lambda function in map()
 tuple_a = (100, 200, 300, 400, 500)
 tuple_b = tuple(map(lambda x: x/100, tuple_a))
-print(tuple_b) #prints '(1, 2, 3, 4, 5)'
+print(tuple_b) #prints '(1.0, 2.0, 3.0, 4.0, 5.0)'
+
+
+# We can directly place iterable in map
+list_c = list(map(lambda x: x * 5, [1, 2, 3, 4, 5]))
+tuple_c = tuple(map(lambda x: x * 4, (2, 5, 9, 14)))
+print(list_c) #prints '[5, 10, 15, 20, 25]'
+print(tuple_c) #prints '(8, 20, 36, 56)'
+
+
+# Other iterables (set, range)
+range_a = list(map(lambda x: x * 3, range(1, 10))) 
+set_a = set(map(lambda x: x - 9, {99, 94, 87, 90}))
+print(range_a) #prints '[3, 6, 9, 12, 15, 18, 21, 24, 27]'
+print(set_a) #prints '{81, 90, 85, 78}'
 
