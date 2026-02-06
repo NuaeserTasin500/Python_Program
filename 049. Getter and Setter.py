72b98dcc4f43202ca2cfa9407ec94409816107a1
NuaeserTasin500 <osman1711362@gmail.com>
2026-02-06 22:42:55 +0600

    Create 049. Getter and Setter.py

---
+++ 049. Getter and Setter.py
@@ -0,0 +1,27 @@
+## Getter and Setter
+class MyClass: 
+    def __init__(self, value):
+        self._value = value
+
+    def show(self):
+        print(f'Value is {self._value}')
+
+    @property
+    def ten_value(self): #getter
+        return 10*self._value
+    
+    @ten_value.setter
+    def ten_value(self, new_value): #setter
+        self._value = new_value
+        return 10*self._value
+    
+    
+obj = MyClass(10)
+print(obj._value) #prints '10'
+print(obj.ten_value) #prints '100'
+obj.show() #prints 'Value is 10'
+print()
+
+# For changing values, it needs setter
+obj.ten_value = 12 #it goes to @ten_value.setter
+print(obj.ten_value) #prints '120' 
