db63bfa691db673fbb550f260ee124b08e3825f1
NuaeserTasin500 <osman1711362@gmail.com>
2026-02-07 11:43:38 +0600

    Update and rename 049. Getter and Setter.py to 049. OOP Getter and Setter.py

--- 049. OOP Getter and Setter.py
+++ 049. OOP Getter and Setter.py
@@ -6,11 +6,11 @@ class MyClass:
     def show(self):
         print(f'Value is {self._value}')
 
-    @property
+    @property #setting getter
     def ten_value(self): #getter
         return 10*self._value
     
-    @ten_value.setter
+    @ten_value.setter #setting setter
     def ten_value(self, new_value): #setter
         self._value = new_value
         return 10*self._value
