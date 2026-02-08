80e18c0ff94b6ef4d75d5cb43d48d34f86347f79
NuaeserTasin500 <osman1711362@gmail.com>
2026-02-08 21:06:42 +0600

    Create 056. help().py

---
+++ 056. help().py
@@ -0,0 +1,13 @@
+## help() <S>
+#In Python programming, The help() function provides interactive documentation about objects, modules, classes, functions, or keywords.
+
+
+a = 10 #an integer
+print(help(a)) #shows interactive documentation of integer
+print()
+b = 10.5 #a float
+print(help(b)) #shows interactive documentation of float 
+
+
+# Sometimes we can use datatype in help() function <AI>
+print(help(list)) #shows interactive documentation of list
