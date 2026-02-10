279d5ce91b2d8d5739628bd37020daac24234f5d
NuaeserTasin500 <osman1711362@gmail.com>
2026-02-10 11:32:26 +0600

    Implement generator and async function examples
    Added examples of generator functions and async functions.

--- 018. Function.py
+++ 018. Function.py
@@ -72,3 +72,58 @@ def even(x):
 
 print(even(1)) #prints 'False'
 print(even(10)) #prints 'True'
+print()
+
+
+
+
+
+# yield keyword
+#yield keyword turns a function into a function generator
+def gen_func():
+    yield "Hello"
+    yield 51
+    yield "Good Bye"
+
+gen_var = gen_func()
+
+for z in gen_var:
+    print(z) #prints 'Hello <newline> 51 <newline> Good Bye'
+print()
+
+#we can use next() function
+def gen_func2():
+    yield "Hello Guys"
+    yield "This is system"
+gen_var2 = gen_func2()
+print(next(gen_var2)) #prints 'Hello Guys'
+print(next(gen_var2)) #prints 'This is system'
+print()
+
+#type of this generator function
+print(type(gen_var2)) #prints '<class 'generator'>'
+print()
+
+#sequential
+def gen_func(m):
+    for i in range(m):
+        yield i  
+
+for n in gen_func(5):
+    print(n) #prints '0 <newline> 1 <newline> 2 <newline> 3 <newline> 4'
+print()
+
+
+
+
+
+# async function
+#in this case we need to import asyncio library
+#we will know about 'import' later; now just remember about asyncio library
+import asyncio
+async def func300():
+    print("Hello")
+    await asyncio.sleep(3) # Pause for 3 second without blocking
+    print("World")
+
+asyncio.run(func300())
