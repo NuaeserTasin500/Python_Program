5a09bad4453a6f3e931ad5dce9982369aafc05d2
NuaeserTasin500 <osman1711362@gmail.com>
2026-03-09 17:16:06 +0600

    Change: version_002
    Added a function to calculate the sum of arbitrary arguments.

--- 018. Function.py
+++ 018. Function.py
@@ -138,3 +138,19 @@ async def func300():
     print("World")
 
 asyncio.run(func300())
+
+
+
+# ================================================ #
+
+
+# Function with Arbitrary Arguments
+
+def sum(*numbers):
+    result = 0
+    for i in numbers:
+        result = result + i
+    return result
+
+summation = sum(1, 2, 3, 4)
+print(summation) #prints '10', the result of 1+2+3+4
